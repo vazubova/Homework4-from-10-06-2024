85db694382a927288c52007fa90403fc6d0642bd
varvaragr <grechishnikova.varvara@gmail.com>
2024-06-09 23:47:11 +0300

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        int age = 19;
 
-}}
+        if (age  >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        }
+
+        else {
+
+            System.out.println("Если возраст человека равен " + age +  ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+    }
+
+
+}
